0e0135131bdb98a1cb0b83509e3ab20ee1b71c14
liheng <tangcuyu@163.com>
2020-05-22 20:24:55 +0800

    拆分结构

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
+import Home from './views/home'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(Home)
 }).$mount('#app')
